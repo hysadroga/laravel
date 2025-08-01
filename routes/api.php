fe14cdf81075f8fdb43d8b95477428c0b4ae9fa7
hysadroga <hhisadorasantos@gmail.com>
2025-08-01 16:28:33 -0300

    aula 01.08

--- routes/api.php
+++ routes/api.php
@@ -18,18 +18,25 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/clientes', function(){
-    return [
-        ['nome' => 'Jussara', 'email' => 'jussara@email.com'],
-        ['nome' => 'Carlos', 'email' => 'carlos@email.com'],
-        ['nome' => 'Mariana', 'email' => 'mariana@email.com'],
-        ['nome' => 'Pedro', 'email' => 'pedro@email.com'],
-        ['nome' => 'Ana', 'email' => 'ana@email.com'],
-        ['nome' => 'Lucas', 'email' => 'lucas@email.com'],
-        ['nome' => 'Fernanda', 'email' => 'fernanda@email.com'],
-        ['nome' => 'Rafael', 'email' => 'rafael@email.com'],
-        ['nome' => 'Beatriz', 'email' => 'beatriz@email.com'],
-        ['nome' => 'Gustavo', 'email' => 'gustavo@email.com'],
-        ['nome' => 'Camila', 'email' => 'camila@email.com'],
-    ];
-});
+Route::apiResource('proprietario', 'App\Http\Controllers\ProprietarioController');
+
+
+
+
+
+//AULA 30/07
+// Route::get('/clientes', function(){
+//     return [
+//         ['nome' => 'Jussara', 'email' => 'jussara@email.com'],
+//         ['nome' => 'Carlos', 'email' => 'carlos@email.com'],
+//         ['nome' => 'Mariana', 'email' => 'mariana@email.com'],
+//         ['nome' => 'Pedro', 'email' => 'pedro@email.com'],
+//         ['nome' => 'Ana', 'email' => 'ana@email.com'],
+//         ['nome' => 'Lucas', 'email' => 'lucas@email.com'],
+//         ['nome' => 'Fernanda', 'email' => 'fernanda@email.com'],
+//         ['nome' => 'Rafael', 'email' => 'rafael@email.com'],
+//         ['nome' => 'Beatriz', 'email' => 'beatriz@email.com'],
+//         ['nome' => 'Gustavo', 'email' => 'gustavo@email.com'],
+//         ['nome' => 'Camila', 'email' => 'camila@email.com'],"
+//     ];
+// });
